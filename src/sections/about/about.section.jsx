7fa3f2f658bd8d8375667ac7cda4d92032d908e4
anujpanwar01 <anujpanwar7911@gmail.com>
2022-06-10 21:49:32 +0530

    save the merge conflicit

--- src/sections/about/about.section.jsx
+++ src/sections/about/about.section.jsx
@@ -106,7 +106,6 @@ function Info(props) {
   }
 
   return (
-<<<<<<< HEAD
     <AboutSection
       id="about"
       ref={infoRef}
@@ -114,56 +113,6 @@ function Info(props) {
     >
       {" "}
       {content}
-=======
-    <AboutSection id="about">
-      <ImageContainer ref={imgRef} currenttheme={currentTheme}>
-        {props.imgError && <p>Img Not loaded. Something went wrong </p>}
-        <img
-          style={{ borderRadius: "0.5rem" }}
-          width={"100%"}
-          src={props.imgUrl}
-          alt="Anuj Panwar"
-        />
-      </ImageContainer>
-      <TextContainer>
-        <SectionHeading currenttheme={currentTheme}>Who I am</SectionHeading>
-        <h4>About Me</h4>
-        <AboutText>
-          Hello, my name is <strong>Anuj Panwar</strong>, Passionate about
-          Front-end Development. I'm from Dehradun Uttarakhand, India. I Love to
-          learn new skills and think creatively I'm a Fresher looking for a job.
-          I build some projects to enhance my skills.
-          <p>
-            I build projects using HTML5, CSS3, SCSS, JAVASCRIPT, and REACT JS
-            and keep always looking for better SEO. I love to create the best
-            visual effects and features that attract users.
-          </p>
-          <p>
-            <strong style={{ color: " #00ff14", fontWeight: "400" }}>
-              {" "}
-              “You define your own life. Don’t let other people write your
-              script.”
-              <br />— Oprah Winfrey
-            </strong>
-          </p>
-        </AboutText>
-        {width <= 700 && <SocialIcons isAboutSection={"about"} />}
-        <CustomLink
-          currenttheme={currentTheme}
-          $navLink="button"
-          target="_blank"
-          rel="noreferrer"
- master
-          to={props.url}
-          // to="https://my.indeed.com/p/anujp-hskl1dp"
-
-          download
-          style={{ fontWeight: "500", fontSize: "1.8rem" }}
-        >
-          Download CV
-        </CustomLink>
-      </TextContainer>
->>>>>>> 888e8572e6e652fd98ed714ce4e035f435827964
     </AboutSection>
   );
 }
